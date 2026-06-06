eefb06a65d94b568e03536a408c9377dd84ed4da
Alex141994 <leonidalex1984@egmail.com>
2026-06-06 03:07:21 +0300

    Build errors are in the src/handlers.cpp file. The class wssSession is extracted from src/main.cpp and located to src/wssSession.hpp and src/handlers.cpp.

---
+++ src/handlers.cpp
@@ -0,0 +1,302 @@
+// handlers.cpp
+#include "wssSession.hpp" // Важно подключить заголовочный файл класса
+#include <iostream>
+#include <regex>
+#include <boost/core/ignore_unused.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+
+// 1. Метод handle_list_request
+void WssSession::handle_list_request(const FilesFoldersListRequest& req) {
+    boost::ignore_unused(req);
+    // Ваша логика здесь
+}
+
+// 2. Метод handle_delete_file
+void WssSession::handle_delete_file(const DeleteFileRequest& req) {
+    boost::ignore_unused(req);
+    // Ваша логика здесь
+}
+
+// 3. Метод handle_files_ids_request
+void WssSession::handle_files_ids_request(const FilesIds& req) {
+    boost::ignore_unused(req);
+    // Ваша логика здесь
+}
+
+// 4. Полная реализация тяжелого handle_path_inf_request
+// void WssSession::handle_path_inf_request(const PathInfoRequest& req) {
+//     auto self = shared_from_this();
+//     auto thread_pool_executor = ws_.get_executor(); 
+
+//     boost::asio::post(thread_pool_executor, [this, self, req]() {
+//         try {
+//             std::string input_path = req.netpath();
+//             std::string users_prefix = "USERS"; 
+//             std::string prefix = users_prefix + "/" + user_id_;
+//             std::string formatted_path = input_path;
+
+//             if (formatted_path.rfind(prefix, 0) != 0) { 
+//                 if (formatted_path.rfind(users_prefix, 0) == 0) {
+//                     formatted_path = prefix + formatted_path.substr(users_prefix.length());
+//                 } else {
+//                     formatted_path = prefix + "/" + formatted_path;
+//                 }
+//             }
+
+//             bool is_explicit_folder = !formatted_path.empty() && formatted_path.back() == '/';
+            
+//             auto escape_regex = [](const std::string& str) {
+//                 std::regex special_chars(R"([-\\^$*+?.()|[\]{}])");
+//                 return std::regex_replace(str, special_chars, R"(\$&)");
+//             };
+
+//             auto mongo_db = db_.get_db(); 
+//             auto collection = mongo_db["ImageRecord"];
+
+//             std::string s3_endpoint = cfg_.get_s3_endpoint(); 
+//             std::string bucket_name = cfg_.get_bucket_name();
+//             int s3_expires = cfg_.get_s3_expires();
+
+//             if (is_explicit_folder) {
+//                 using bsoncxx::builder::stream::document;
+//                 using bsoncxx::builder::stream::finalize;
+
+//                 std::string pattern = "^" + escape_regex(formatted_path);
+//                 auto query = document{} << "folder" << bsoncxx::types::b_regex{pattern} << finalize;
+                
+//                 mongocxx::options::find opts{};
+//                 opts.projection(document{} << "_id" << 1 << finalize);
+
+//                 auto doc = collection.find_one(query.view(), opts);
+
+//                 if (doc) {
+//                     boost::asio::post(ws_.get_executor(), [this, self, input_path, s3_endpoint, bucket_name]() {
+//                         // Вызов другого метода этого же класса
+//                         this->handle_list_request(input_path); 
+
+//                         ServerEnvelope response;
+//                         response.set_type(ServerEnvelope_Type_SERVER_MESSAGE);
+//                         auto* path_resp = response.mutable_pathinfresponse();
+//                         path_resp->set_netpath(s3_endpoint + "/" + bucket_name + "/" + input_path);
+//                         path_resp->set_netstorepath("");
+//                         path_resp->set_result("folder");
+
+//                         this->send_envelope(response);
+//                     });
+//                     return;
+//                 } else {
+//                     send_not_exist_response_async(input_path, s3_endpoint, bucket_name);
+//                     return;
+//                 }
+//             }
+
+//             // ... Остальные кейсы (папка без слэша, файл, not exist) из предыдущего ответа ...
+//             // Везде внутри лямбды "this->" и "send_not_exist_response_async" будут работать корректно, 
+//             // так как это код метода класса WssSession.
+
+//         } catch (const std::exception& e) {
+//             std::cerr << "Exception in handle_path_inf_request thread: " << e.what() << std::endl;
+//         }
+//     });
+// }
+
+// src/handlers.cpp
+
+void WssSession::handle_path_inf_request(const PathInfoRequest& req) {
+    // Захватываем shared_ptr на текущую сессию, чтобы предотвратить её деструкцию
+    auto self = shared_from_this();
+
+    // Получаем исполнителя (thread pool / io_context) для фонового выполнения
+    auto thread_pool_executor = ws_.get_executor(); 
+
+    // Переносим тяжелую блокирующую логику (БД и AWS S3) в пул потоков Asio
+    boost::asio::post(thread_pool_executor, [this, self, req]() {
+        try {
+            std::string input_path = req.netpath();
+            std::string users_prefix = "USERS"; 
+            std::string prefix = users_prefix + "/" + user_id_;
+            std::string formatted_path = input_path;
+
+            // 1. Обеспечиваем наличие префикса в пути
+            if (formatted_path.rfind(prefix, 0) != 0) { 
+                if (formatted_path.rfind(users_prefix, 0) == 0) {
+                    formatted_path = prefix + formatted_path.substr(users_prefix.length());
+                } else {
+                    formatted_path = prefix + "/" + formatted_path;
+                }
+            }
+
+            bool is_explicit_folder = !formatted_path.empty() && formatted_path.back() == '/';
+            
+            // Лямбда для экранирования спецсимволов в regex (аналог escapeRegex)
+            auto escape_regex = [](const std::string& str) {
+                std::regex special_chars(R"([-\\^$*+?.()|[\]{}])");
+                return std::regex_replace(str, special_chars, R"(\$&)");
+            };
+
+            // Извлекаем базу данных из вашей обертки db_
+            auto mongo_db = db_.get_db(); 
+            auto collection = mongo_db["ImageRecord"];
+
+            // ИСПОЛЬЗОВАНИЕ ВАШЕГО СТРУКТУРЫ CONFIG напрямую через публичные поля:
+            std::string s3_endpoint = cfg_.s3_endpoint; 
+            std::string bucket_name = cfg_.s3_bucket;
+            int s3_expires = 360; // Значение по умолчанию (в Config этого поля нет)
+
+            // --- КЕЙС 1: Путь заканчивается на "/" ---
+            if (is_explicit_folder) {
+                using bsoncxx::builder::stream::document;
+                using bsoncxx::builder::stream::finalize;
+
+                std::string pattern = "^" + escape_regex(formatted_path);
+                auto query = document{} << "folder" << bsoncxx::types::b_regex{pattern} << finalize;
+                
+                mongocxx::options::find opts{};
+                opts.projection(document{} << "_id" << 1 << finalize);
+
+                auto doc = collection.find_one(query.view(), opts);
+
+                if (doc) {
+                    // Возвращаем вызовы в поток сокета (I/O thread) для безопасной отправки
+                    boost::asio::post(ws_.get_executor(), [this, self, input_path, s3_endpoint, bucket_name]() {
+                        FilesFoldersListRequest list_req;
+                        list_req.set_foldername(input_path);
+                        this->handle_list_request(list_req); 
+
+                        ServerEnvelope response;
+                        response.set_type(ServerEnvelope_Type_SERVER_MESSAGE);
+                        auto* path_resp = response.mutable_pathinfresponse();
+                        path_resp->set_netpath(s3_endpoint + "/" + bucket_name + "/" + input_path);
+                        path_resp->set_netstorepath("");
+                        path_resp->set_result("folder");
+
+                        this->send_envelope(response);
+                    });
+                    return;
+                } else {
+                    send_not_exist_response_async(input_path, s3_endpoint, bucket_name);
+                    return;
+                }
+            }
+
+            // --- КЕЙС 2: Проверка папки без явного слэша ---
+            std::string folder_with_slash = formatted_path + "/";
+            {
+                using bsoncxx::builder::stream::document;
+                using bsoncxx::builder::stream::finalize;
+
+                std::string pattern = "^" + escape_regex(folder_with_slash);
+                auto folder_query = document{} << "folder" << bsoncxx::types::b_regex{pattern} << finalize;
+                
+                mongocxx::options::find opts{};
+                opts.projection(document{} << "_id" << 1 << finalize);
+
+                auto folder_doc = collection.find_one(folder_query.view(), opts);
+
+                if (folder_doc) {
+                    boost::asio::post(ws_.get_executor(), [this, self, input_path, s3_endpoint, bucket_name]() {
+                        this->handle_list_request(input_path);
+
+                        ServerEnvelope response;
+                        response.set_type(ServerEnvelope_Type_SERVER_MESSAGE);
+                        auto* path_resp = response.mutable_pathinfresponse();
+                        path_resp->set_netpath(s3_endpoint + "/" + bucket_name + "/" + input_path + "/");
+                        path_resp->set_netstorepath("");
+                        path_resp->set_result("folder");
+
+                        this->send_envelope(response);
+                    });
+                    return;
+                }
+            }
+
+            // --- КЕЙС 3: Поиск файла по s3Key или комбинации (folder + originalName) ---
+            {
+                using bsoncxx::builder::stream::document;
+                using bsoncxx::builder::stream::open_array;
+                using bsoncxx::builder::stream::close_array;
+                using bsoncxx::builder::stream::open_document;
+                using bsoncxx::builder::stream::close_document;
+                using bsoncxx::builder::stream::finalize;
+
+                auto file_query = document{} 
+                    << "$or" << open_array 
+                        << open_document << "s3Key" << formatted_path << close_document
+                        << open_document 
+                            << "$expr" << open_document 
+                                << "$eq" << open_array 
+                                    << open_document << "$concat" << open_array << "$folder" << "$originalName" << close_array << close_document
+                                    << formatted_path 
+                                << close_array 
+                            << close_document 
+                        << close_document
+                    << close_array 
+                    << finalize;
+
+                mongocxx::options::find opts{};
+                opts.projection(document{} 
+                    << "_id" << 1 
+                    << "s3Key" << 1 
+                    << "originalName" << 1 
+                    << "size" << 1 
+                    << finalize);
+
+                auto file_doc = collection.find_one(file_query.view(), opts);
+
+                if (file_doc) {
+                    auto view = file_doc->view();
+                    std::string s3_key = view["s3Key"].get_string().value.to_string();
+                    std::string original_name = view["originalName"].get_string().value.to_string();
+                    int64_t file_size = view["size"] ? view["size"].get_int64().value : 0;
+                    std::string mongo_id = view["_id"].get_oid().value.to_string();
+
+                    // Генерация AWS S3 Signed URL
+                    Aws::Http::URI uri = s3_client_.GeneratePresignedUrl(
+                        bucket_name, 
+                        s3_key, 
+                        Aws::Http::HttpMethod::HTTP_GET, 
+                        s3_expires
+                    );
+                    std::string signed_url = uri.GetURIString();
+
+                    // Безопасно передаем данные в поток I/O для отправки клиенту
+                    boost::asio::post(ws_.get_executor(), [this, self, original_name, mongo_id, signed_url, file_size]() {
+                        ServerEnvelope response;
+                        response.set_type(ServerEnvelope_Type_SERVER_MESSAGE);
+                        auto* files_resp = response.mutable_filesidsresponse();
+                        
+                        auto* file_info = files_resp->add_files(); 
+                        file_info->set_filename(original_name);
+                        file_info->set_mongoid(mongo_id);
+                        file_info->set_url(signed_url);
+                        file_info->set_size(file_size);
+
+                        this->send_envelope(response);
+                    });
+                    return;
+                }
+            }
+
+            // --- КЕЙС 4: Ничего не найдено ---
+            send_not_exist_response_async(input_path, s3_endpoint, bucket_name);
+
+        } catch (const std::exception& e) {
+            std::cerr << "Exception in handle_path_inf_request thread: " << e.what() << std::endl;
+        }
+    });
+}
+
+// Хелпер тоже выносим в handlers.cpp
+void WssSession::send_not_exist_response_async(const std::string& input_path, const std::string& s3_endpoint, const std::string& bucket_name) {
+    auto self = shared_from_this();
+    boost::asio::post(ws_.get_executor(), [this, self, input_path, s3_endpoint, bucket_name]() {
+        ServerEnvelope response;
+        response.set_type(ServerEnvelope_Type_SERVER_MESSAGE);
+        auto* path_resp = response.mutable_pathinfresponse();
+        path_resp->set_netpath(s3_endpoint + "/" + bucket_name + "/" + input_path + "/");
+        path_resp->set_netstorepath("");
+        path_resp->set_result("not exist");
+
+        this->send_envelope(response);
+    });
+}
---
+++ src/wssSession.hpp
@@ -0,0 +1,206 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/ssl.hpp>
+
+#include <iostream>
+#include <string>
+#include <regex>
+#include <chrono>
+#include <future>
+#include <nlohmann/json.hpp>
+
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/json.hpp>
+#include <bsoncxx/oid.hpp>
+#include <mongocxx/instance.hpp>
+
+#include <aws/core/Aws.h>
+#include <aws/s3/S3Client.h>
+#include <aws/s3/model/PutObjectRequest.h>
+
+#include "image.pb.h"
+#include "config.hpp"
+#include "database.hpp"
+#include "aux_handler.hpp"
+
+namespace asio = boost::asio;
+namespace ssl = boost::asio::ssl;
+namespace beast = boost::beast;
+namespace http = boost::beast::http;
+namespace websocket = boost::beast::websocket;
+using tcp = boost::asio::ip::tcp;
+using json = nlohmann::json;
+
+inline std::string url_decode(const std::string& in);
+inline std::map<std::string, std::string> parse_form_body(const std::string& body);
+
+// --- СЛОЙ 1: WEB SOCKET SECURE SESSION (Порт 8080) ---
+class WssSession : public std::enable_shared_from_this<WssSession> {
+    // Настраиваем тип WebSocket-стрима на точное совпадение с типом из HTTP-сессии
+    websocket::stream<beast::ssl_stream<tcp::socket>> ws_;
+    beast::flat_buffer buffer_;
+    Database& db_;
+    Aws::S3::S3Client& s3_client_;
+    Config& cfg_;
+    std::string user_id_;
+    http::request<http::string_body> upgrade_req_;
+
+public:
+    // Конструктор принимает УЖЕ ГОТОВЫЙ, полностью рабочий SSL-стрим,
+    // в котором Qt-клиент уже успешно прошел шифрование!
+    WssSession(beast::ssl_stream<tcp::socket> ssl_stream, Database& db, Aws::S3::S3Client& s3, Config& cfg, std::string uid)
+        : ws_(std::move(ssl_stream)), db_(db), s3_client_(s3), cfg_(cfg), user_id_(uid) {}
+
+    void start(http::request<http::string_body> req) {
+        upgrade_req_ = std::move(req);
+        
+        // Отключаем таймауты Beast на время хэндшейка сокетов
+        //  beast::get_lowest_layer(ws_).expires_never();
+        
+        // Запускаем асинхронный WebSocket Handshake прямо поверх живого SSL
+        ws_.async_accept(upgrade_req_, beast::bind_front_handler(&WssSession::on_accept, shared_from_this()));
+    }
+
+private:
+    void on_accept(beast::error_code ec) {
+        if (ec) {
+            std::cerr << "WSS Accept Error: " << ec.message() << std::endl;
+            return;
+        }
+        do_read();
+    }
+
+    void do_read() {
+        buffer_.clear();
+        ws_.async_read(buffer_, beast::bind_front_handler(&WssSession::on_read, shared_from_this()));
+    }
+
+    void on_read(beast::error_code ec, std::size_t bytes_transferred) {
+        boost::ignore_unused(bytes_transferred);
+        if (ec == websocket::error::closed) return;
+        if (ec) return;
+
+        if (ws_.got_binary()) {
+            std::string raw_data = beast::buffers_to_string(buffer_.data());
+            ClientEnvelope envelope;
+            if (envelope.ParseFromString(raw_data)) {
+                dispatch_protobuf_message(envelope);
+            }
+        }
+    }
+
+    void dispatch_protobuf_message(const ClientEnvelope& envelope) {
+        if (envelope.type() != ClientEnvelope_Type_CLIENT_MESSAGE) {
+            std::cerr << "Received non-client message envelope." << std::endl;
+            send_error("Unhandled or unknown envelope type.");
+            return;
+        }
+
+        if (envelope.has_addfile()) {
+            std::cout << "Received AddFileRequest from user " << user_id_ << std::endl;
+            handle_add_file(envelope.addfile());
+        } 
+        else if (envelope.has_listrequest()) {
+            std::cout << "Received FilesFoldersListRequest from user " << user_id_ << std::endl;
+            handle_list_request(envelope.listrequest());
+        } 
+        else if (envelope.has_deletefile()) {
+            std::cout << "Received DeleteFileRequest from user " << user_id_ << std::endl;
+            handle_delete_file(envelope.deletefile());
+        } 
+        else if (envelope.has_filesidsrequest()) {
+            std::cout << "Received FilesIdsRequest from user " << user_id_ << std::endl;
+            handle_files_ids_request(envelope.filesidsrequest());
+        } 
+        else if (envelope.has_pathinfrequest()) {
+            std::cout << "Received PathInfoRequest from user " << user_id_ << std::endl;
+            handle_path_inf_request(envelope.pathinfrequest());
+        } 
+        else {
+            std::cerr << "Received unknown or empty client message from user " << user_id_ << std::endl;
+            send_error("Empty or unhandled packet action.");
+        }
+    }
+
+    void handle_add_file(const AddFileRequest& req) {
+        try {
+            std::string filename = req.filename();
+            std::string folder = req.folder();
+            std::string file_data = req.data();
+
+            std::string sanitized_folder = AuxHandler::sanitize_to_path(folder);
+            auto [unique_name, ext] = AuxHandler::prepare_filename(filename);
+            
+            std::string target_folder = sanitized_folder.empty() ? ("users/" + user_id_ + "/") : sanitized_folder + "/";
+            std::string s3_key = target_folder + unique_name;
+
+            Aws::S3::Model::PutObjectRequest s3_req;
+            s3_req.SetBucket(cfg_.s3_bucket.c_str());
+            s3_req.SetKey(s3_key.c_str());
+            s3_req.SetBody(std::make_shared<std::stringstream>(file_data));
+            if (!s3_client_.PutObject(s3_req).IsSuccess()) {
+                send_error("MinIO write failed");
+                return;
+            }
+
+            auto users_col = db_.get_db()["users"];
+            auto user_doc = users_col.find_one(bsoncxx::builder::stream::document{} << "_id" << bsoncxx::oid{user_id_} << bsoncxx::builder::stream::finalize);
+            
+            std::string usr_login = "Unknown";
+            if (user_doc) {
+                usr_login = std::string(user_doc->view()["login"].get_string().value);
+            }
+
+            auto records_col = db_.get_db()["imagerecords"];
+            auto doc = bsoncxx::builder::stream::document{}
+                << "name" << unique_name
+                << "originalName" << filename
+                << "folder" << target_folder
+                << "s3Key" << s3_key
+                << "bucket" << cfg_.s3_bucket
+                << "userLogin" << usr_login
+                << "size" << static_cast<int64_t>(file_data.size())
+                << bsoncxx::builder::stream::finalize;
+            records_col.insert_one(doc.view());
+
+            ServerEnvelope resp;
+            resp.set_type(ServerEnvelope_Type_SERVER_MESSAGE);
+            ServerResponse* s_res = resp.mutable_serverresp();
+            s_res->set_content("upload_result");
+            s_res->set_status("success");
+            send_envelope(resp);
+        } catch (const std::exception& e) {
+            send_error(e.what());
+        }
+    }
+
+    void handle_list_request(const FilesFoldersListRequest& req);   // { boost::ignore_unused(req); }
+    void handle_delete_file(const DeleteFileRequest& req);  // { boost::ignore_unused(req); }
+    void handle_files_ids_request(const FilesIds& req);     // { boost::ignore_unused(req); }
+    void handle_path_inf_request(const PathInfoRequest& req);   // { boost::ignore_unused(req); }
+
+    void send_envelope(const ServerEnvelope& env) {
+        std::string out;
+        if (env.SerializeToString(&out)) {
+            ws_.binary(true);
+            auto shared_data = std::make_shared<std::string>(std::move(out));
+            ws_.async_write(asio::buffer(*shared_data), [self = shared_from_this(), shared_data](beast::error_code ec, std::size_t) {
+                if (!ec) self->do_read();
+            });
+        }
+    }
+
+    void send_error(const std::string& msg) {
+        ServerEnvelope resp;
+        resp.set_type(ServerEnvelope_Type_SERVER_MESSAGE);
+        ServerResponse* s_res = resp.mutable_serverresp();
+        s_res->set_content(msg);
+        s_res->set_status("error");
+        send_envelope(resp);
+    }
+};
+
